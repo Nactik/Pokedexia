0bbceca79c91ccb660a38d70e1f2dcf7969e78ee
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-08-23 22:19:28 +0200

    add DraggableScrollableSheet for pokemon Info

--- lib/PokemonInfoPage.dart
+++ lib/PokemonInfoPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokeapi/model/pokemon/pokemon.dart';
 import 'package:pokeapi/pokeapi.dart';
+import 'package:pokedexia/Utils.dart';
 
 class PokemonInfoPage extends StatelessWidget {
   final Pokemon _pokemon;
@@ -9,19 +10,51 @@ class PokemonInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Utils.getColorFromType(_pokemon.types[0].type.name),
       appBar: AppBar(
-        actions: [Icon(Icons.arrow_back)],
         title: Text(_pokemon.name),
-        backgroundColor: Colors.white,
+        backgroundColor: Utils.getColorFromType(_pokemon.types[0].type.name),
         elevation: 0,
       ),
       body: SafeArea(
-          child: Container(
+          child: Stack(
         alignment: Alignment.center,
-        child: Hero(
-          tag: _pokemon.id,
-          child: Image(image: NetworkImage(_pokemon.sprites.frontDefault)),
-        ),
+        children: [
+          Positioned(
+            top: 0,
+            child: Hero(
+              tag: _pokemon.id,
+              child: Image(image: NetworkImage(_pokemon.sprites.frontDefault)),
+            ),
+          ),
+          DraggableScrollableSheet(
+            expand: true,
+            initialChildSize: 0.8,
+            minChildSize: 0.8,
+            maxChildSize: 1.0,
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                  color: Colors.white,
+                ),
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    children: [
+                      Card(child: Text("coucou")),
+                      Card(child: Text("coucou")),
+                      Card(child: Text("coucou")),
+                      Card(child: Text("coucou")),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
       )),
     );
   }
